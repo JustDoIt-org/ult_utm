d8629cda7ebb99848d079f63ee3c1a01826b6f81
Andy Eka Mulya <andymulya13@gmail.com>
2024-04-25 19:38:57 +0700

    create navbar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Tailwind Test</title>
-  @vite('resources/css/app.css')
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Tailwind Test</title>
+        @vite('resources/css/app.css')
+    </head>
 
-<body>
-  <h1 class="h-4 text-red-600">Hello</h1>
-</body>
+    <body class="bg-blue-100">
+        <header>
+            <x-navbar-menu />
+        </header>
+    </body>
 
 </html>
---
+++ resources/views/components/navbar-menu.blade.php
@@ -0,0 +1,10 @@
+<nav class="flex items-center justify-between h-14 bg-white p-5">
+    <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+            <path fill-rule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+        </svg>
+    </div>
+
+    <h1 class="font-semibold text-xl text-blue-950">Dashboard</h1>
+    <div class="bg-blue-500 w-10 h-10 rounded-full flex justify-center items-center font-semibold text-white">A</div>
+</nav>
