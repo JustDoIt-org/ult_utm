612f032236db18f3ca87ccb9ce805092e4bb9527
Andy Eka Mulya <andymulya13@gmail.com>
2024-04-27 12:34:54 +0700

    change the color of the navigation section when active to blue

--- resources/views/components/link-nav.blade.php
+++ resources/views/components/link-nav.blade.php
@@ -1,13 +1,11 @@
 @props(["link" => "", "nameLink" => "Link"])
 
 @php
-    $classes = (request()->routeIs($link)) ? "flex items-center gap-5 p-5 border-l-[6px] border-blue-500" : "flex items-center gap-5 p-5"
+    $classes = (request()->routeIs($link)) ? "flex items-center gap-5 p-5 border-l-[6px] border-blue-500 font-semibold text-blue-700" : "flex items-center gap-5 p-5 font-semibold text-slate-500"
 @endphp
 
 <a wire:navigate href="{{ route($link) }}" {{ $attributes->merge(['class' => $classes]) }}>
-    <div class="font-bold text-blue-700">
-        {{ $slot }}
-    </div>
-    <span class="font-semibold">{{ $nameLink }}</span>
+    <span>{{ $slot }}</span>
+    <span>{{ $nameLink }}</span>
 </a>
 
