0c7c1b0eef2db01ab7de7a39333dc64b8702797c
Alvin Maulana Shobari <alvinms1001@gmail.com>
2024-04-29 16:00:55 +0700

    ult-layout

--- resources/views/components/layouts/ult-layout.blade.php
+++ resources/views/components/layouts/ult-layout.blade.php
@@ -5,9 +5,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ULT | Universitas Trunojoyo Madura</title>
-  <script src="https://cdn.tailwindcss.com"></script>
+  {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+
   <script src="https://kit.fontawesome.com/8eec8f5ed5.js" crossorigin="anonymous"></script>
-  <script src="//unpkg.com/alpinejs" defer></script>
+  {{-- <script src="//unpkg.com/alpinejs" defer></script> --}}
+  <livewire:styles />
 </head>
 
 
@@ -16,6 +19,8 @@
   {{ $slot }}
 
   <x-layouts.partials.footer.footer-ult :$items />
+
+  <livewire:scripts />
 </body>
 
 </html>
