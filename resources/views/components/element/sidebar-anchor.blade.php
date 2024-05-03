95539c82c840429387bdd9f45f55dc993c43b0c1
Andy Eka Mulya <andymulya13@gmail.com>
2024-05-03 18:59:27 +0700

    Membuat design tampilan dropdown

--- resources/views/components/element/sidebar-anchor.blade.php
+++ resources/views/components/element/sidebar-anchor.blade.php
@@ -1,7 +1,7 @@
 @props(['route' => 'javascript:void(0)'])
 
 @php
-    $classes = (Route::is($route)) ? "flex gap-5 p-4 font-semibold border-l-4 border-yellow-500 bg-yellow-50 text-yellow-800" : "flex gap-5 p-4 text-slate-400 font-semibold hover:bg-slate-50 hover:text-slate-600"
+    $classes = (Route::is($route)) ? "flex gap-5 p-4 font-semibold border-l-4 border-yellow-500 bg-yellow-50 text-yellow-600" : "flex gap-5 p-4 text-slate-400 font-semibold hover:bg-slate-50 hover:text-slate-600"
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}
