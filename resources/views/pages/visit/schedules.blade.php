0de2c763e106fa7ffa2f664a0180789c75236ce1
Andy Eka Mulya <andymulya13@gmail.com>
2024-05-07 15:19:21 +0700

    Membuat calendar di page schedule dan memperbaiki tampilan responsive pada page auth

--- resources/views/pages/visit/schedules.blade.php
+++ resources/views/pages/visit/schedules.blade.php
@@ -1,5 +1,25 @@
 @extends('components.layouts.multiple')
 
+<!-- Full Calendar -->
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+       var calendarEl = document.getElementById('calendar');
+       var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth'
+       });
+       calendar.render();
+     });
+</script>
+
 @section('content')
-    <h1>Schedules</h1>
+    <section class="flex flex-col gap-10 px-5 pt-10">
+        <h1 class="font-bold text-3xl">Visitor</h1>
+
+        <div class="bg-white px-5 py-10 rounded-xl shadow-lg">
+            <h2 class="font-semibold text-xl">Informasi Kouta</h2>
+            <div></div>
+        </div>
+
+        <div id="calendar" class="bg-white px-5 py-10 rounded-lg shadow-lg"></div>
+    </section>
 @endsection
