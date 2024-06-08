3c7c8f8da24ea890f959cad3a669ae4228523083
Andy Eka Mulya <andymulya13@gmail.com>
2024-06-08 07:26:55 +0700

    Menambahkan fitur download pada pengajuan kunjungan untuk user dan memperbaiki tampilannya

--- resources/views/pages/admin/visit/pengajuan-kunjungan/pengajuan-kunjungan-form-modal.blade.php
+++ resources/views/pages/admin/visit/pengajuan-kunjungan/pengajuan-kunjungan-form-modal.blade.php
@@ -70,9 +70,9 @@
     <section>
         <div x-data="{ uploading: false, progress: 0 }" x-on:livewire-upload-start="uploading = true"
             x-on:livewire-upload-finish="uploading = false" x-on:livewire-upload-cancel="uploading = false"
-            x-on:livewire-upload-error="uploading = false" x-on:livewire-upload-progress="progress = $event.detail.progress" wire:target="save">
+            x-on:livewire-upload-error="uploading = false" x-on:livewire-upload-progress="progress = $event.detail.progress" wire:target="save" class="flex flex-col gap-2">
 
-            <div class="flex gap-2 mb-2">
+            <div class="relative">
                 <x-element.layout.vertical name="form.surat_permohonan" label="Surat Permohonan">
                     <input x-show="!uploading" type="file" accept=".pdf, .doc, .docx" id="form.surat_permohonan" wire:model="form.surat_permohonan" />
                     <div x-show="uploading" class="flex justify-center">
@@ -81,11 +81,14 @@
                 </x-element.layout.vertical>
 
                 {{-- Download file --}}
-                <button type="button" wire:click="downloadFile" class="p-1 text-yellow-500 rounded-lg hover:bg-yellow-100">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
-                    </svg>
-                </button>
+                @if ($form->id != 0)
+                    <button type="button" wire:click="downloadFile" class="flex flex-row gap-1 items-center font-semibold text-sm p-1 text-yellow-500 rounded-lg hover:bg-yellow-100 absolute top-0 right-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
+                        </svg>
+                        <span>Download</span>
+                    </button>
+                @endif
             </div>
 
             <section>
