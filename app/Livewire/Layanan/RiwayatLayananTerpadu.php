683575d5fcce599b1b92a40d1b1cf1921605fae0
Alvin Maulana Shobari <alvinms1001@gmail.com>
2024-08-19 14:20:54 +0700

    fix: riwayat layanan terpadu

--- app/Livewire/Layanan/RiwayatLayananTerpadu.php
+++ app/Livewire/Layanan/RiwayatLayananTerpadu.php
@@ -104,7 +104,7 @@ class RiwayatLayananTerpadu extends Component
                 } else {
                     $user_id = Auth::id();
                     $admin_layanan = AdminLayananModel::where('user_id', $user_id)->get();
-                    $tujuan = $admin_layanan[0]->jenisLayanan->type;
+                    $tujuan = isset($admin_layanan[0]->jenisLayanan->type) ? $admin_layanan[0]->jenisLayanan->type : 'Layanan Akademik';
                     $dataTables = LayananTerpadu::where('progress', 'selesai')->where('tujuan', $tujuan)->get();
                 }
 
