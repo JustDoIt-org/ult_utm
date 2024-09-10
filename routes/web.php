92e954e64cb113c29c232b9b3f14ac200c72c427
Alvin Maulana Shobari <alvinms1001@gmail.com>
2024-09-10 12:19:11 +0700

    fetr

--- routes/web.php
+++ routes/web.php
@@ -35,83 +35,84 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 |
 */
 
-Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function () {
-    Route::get('/', [ultController::class, 'index'])->name('home');
-    Route::match(['get', 'post'], '/botman', 'App\Http\Controllers\ChatBot\ChatBotController@handle');
+Route::get('/', [ultController::class, 'index'])->name('home');
+Route::match(['get', 'post'], '/botman', 'App\Http\Controllers\ChatBot\ChatBotController@handle');
 
-    // Admin
-    Route::prefix('admin')->middleware(['auth', 'verified', 'can:dashboard index'])->group(function () {
-        Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-        Route::get('/users', [UserController::class, 'index'])->can('user index')->name('admin.users');
-        Route::get('/roles', [RoleController::class, 'index'])->can('role index')->name('admin.roles');
-        Route::get('/faqs', [FaqController::class, 'index'])->can('faq index')->name('admin.faqs');
-        Route::get('/faculties', [FacultyController::class, 'index'])->name('admin.faculties');
-        Route::get('/ult-informations', [ULTInformationsController::class, 'index'])->can('ult-informations index')->name('admin.ult-informations');
+// Admin
+Route::prefix('admin')->middleware(['auth', 'verified', 'can:dashboard index'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::get('/users', [UserController::class, 'index'])->can('user index')->name('admin.users');
+    Route::get('/roles', [RoleController::class, 'index'])->can('role index')->name('admin.roles');
+    Route::get('/faqs', [FaqController::class, 'index'])->can('faq index')->name('admin.faqs');
+    Route::get('/faculties', [FacultyController::class, 'index'])->name('admin.faculties');
+    Route::get('/ult-informations', [ULTInformationsController::class, 'index'])->can('ult-informations index')->name('admin.ult-informations');
 
-        Route::get('/request', [PpidAdminController::class, 'request'])->can('ppid index')->name('admin.request');
-        Route::get('/aspirasipengaduan', [PpidAdminController::class, 'aspirasi_pengaduan'])->can('ppid index')->name('admin.aspirasi-pengaduan');
-        Route::get('/survey', [PpidAdminController::class, 'survey'])->can('ppid index')->name('admin.survey');
+    Route::get('/request', [PpidAdminController::class, 'request'])->can('ppid index')->name('admin.request');
+    Route::get('/aspirasipengaduan', [PpidAdminController::class, 'aspirasi_pengaduan'])->can('ppid index')->name('admin.aspirasi-pengaduan');
+    Route::get('/survey', [PpidAdminController::class, 'survey'])->can('ppid index')->name('admin.survey');
 
-        Route::get('/informasi-kouta', [InformasiKoutaController::class, 'index'])->can('informasi-kouta index')->name('admin.informasi-kouta');
-        Route::get('/pengajuan-kunjungan', [PengajuanKunjunganController::class, 'index'])->can('pengajuan-kunjungan index')->name('admin.pengajuan-kunjungan');
-    });
-
-    // Visit
-    Route::get('/visit', [ScheduleController::class, 'index'])->name('visit.schedules');
-    Route::get('/visit/submissions', [SubmissionController::class, 'index'])->name('visit.submissions');
-    Route::middleware(['auth', 'verified'])->get('/visit/cek-absensi', [CekAbsensiController::class, 'index'])->name('visit.cek-absensi');
-
-    Route::post('/message/send', [CekAbsensiController::class, 'sendMessage'])->name('visit.send');
-
-
-    //Konfirmasi Absensi
-    Route::get('/konfirmasi-absensi/{kode}', [KonfirmasiAbsensiController::class, 'index'])->name('konfirmasi-absensi');
-
-    // Ppid
-    Route::middleware(['auth', 'verified'])->prefix('ppid')->group(function () {
-        Route::get('/', [PpidController::class, 'request'])->name('ppid.request');
-        Route::get('/form_keberatan', [PpidController::class, 'keberatan'])->name('ppid.keberatan');
-        Route::get('/aspirasi_pengaduan', [PpidController::class, 'aspirasi_pengaduan'])->name('ppid.aspirasi-pengaduan');
-        Route::get('/survey', [PpidController::class, 'survey'])->name('ppid.survey');
-    });
-
-
-    Route::middleware(['auth', 'verified'])->prefix('terpadu')->group(function () {
-        Route::get('/', [LayananTerpaduController::class, 'index'])->name('lt.home');
-        Route::get('/riwayat', [LayananTerpaduController::class, 'riwayat'])->name('lt.riwayat');
-
-
-        Route::get('/list', [LayananTerpaduController::class, 'list'])->name('lt.list');
-        Route::delete('/list', [LayananTerpaduController::class, 'destroy'])->name('lt.list_delete');
-
-
-        Route::get('/chat_layanan', [ChatController::class, 'chat_guest'])->name('lt.chat_layanan');
-    });
-
-    Route::middleware(['auth', 'verified', 'can:admin-layanan-terpadu index'])->prefix('terpadu')->group(function () {
-        Route::get('/dashboard', [LayananTerpaduController::class, 'dashboard'])->name('lt.dashboard');
-        Route::get('/admin_riwayat', [LayananTerpaduController::class, 'riwayat_admin'])->name('lt.admin_riwayat');
-        Route::delete('/delete_layanan', [LayananTerpaduController::class, 'destroy'])->name('lt.delete_layanan');
-        Route::put('/update_layanan', [LayananTerpaduController::class, 'update'])->name('lt.update_layanan');
-        // Route::get('/atur_admin_layanan', [LayananTerpaduController::class, 'atur_admin'])->name('lt.atur_admin_layanan');
-        // Route::get('/list_layanan', [LayananTerpaduController::class, 'list_layanan'])->name('lt.list_layanan');
-    });
-
-    Route::middleware(['auth', 'verified', 'can:layanan-terpadu index'])->prefix('terpadu')->group(function () {
-        Route::put('/update_layanan_forward', [LayananTerpaduController::class, 'forward'])->name('lt.forward');
-        Route::get('/chat_layanan_admin', [ChatController::class, 'chat_admin'])->name('lt.chat_layanan_admin');
-        Route::get('/chat_admin_to_guest/{id}', [ChatController::class, 'chat_admin_guest'])->name('lt.chat_layanan_admin_guest');
-        Route::delete('/atur_admin_layanan', [LayananTerpaduController::class, 'delete_admin'])->name('lt.update_admin_layanan');
-        Route::resource('/atur_admin_layanan', AdminLTController::class)->name('index', 'lt.atur_admin_layanan');
-        Route::resource('/list_layanan', ListLayananController::class)->name('index', 'lt.list_layanan');
-    });
-
-
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', \App\Livewire\Profile\ProfilePage::class)->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
-
-    require __DIR__ . '/auth.php';
+    Route::get('/informasi-kouta', [InformasiKoutaController::class, 'index'])->can('informasi-kouta index')->name('admin.informasi-kouta');
+    Route::get('/pengajuan-kunjungan', [PengajuanKunjunganController::class, 'index'])->can('pengajuan-kunjungan index')->name('admin.pengajuan-kunjungan');
 });
+
+// Visit
+Route::get('/visit', [ScheduleController::class, 'index'])->name('visit.schedules');
+Route::get('/visit/submissions', [SubmissionController::class, 'index'])->name('visit.submissions');
+Route::middleware(['auth', 'verified'])->get('/visit/cek-absensi', [CekAbsensiController::class, 'index'])->name('visit.cek-absensi');
+
+Route::post('/message/send', [CekAbsensiController::class, 'sendMessage'])->name('visit.send');
+
+
+//Konfirmasi Absensi
+Route::get('/konfirmasi-absensi/{kode}', [KonfirmasiAbsensiController::class, 'index'])->name('konfirmasi-absensi');
+
+// Ppid
+Route::middleware(['auth', 'verified'])->prefix('ppid')->group(function () {
+    Route::get('/', [PpidController::class, 'request'])->name('ppid.request');
+    Route::get('/form_keberatan', [PpidController::class, 'keberatan'])->name('ppid.keberatan');
+    Route::get('/aspirasi_pengaduan', [PpidController::class, 'aspirasi_pengaduan'])->name('ppid.aspirasi-pengaduan');
+    Route::get('/survey', [PpidController::class, 'survey'])->name('ppid.survey');
+});
+
+
+Route::middleware(['auth', 'verified'])->prefix('terpadu')->group(function () {
+    Route::get('/', [LayananTerpaduController::class, 'index'])->name('lt.home');
+    Route::get('/riwayat', [LayananTerpaduController::class, 'riwayat'])->name('lt.riwayat');
+
+
+    Route::get('/list', [LayananTerpaduController::class, 'list'])->name('lt.list');
+    Route::delete('/list', [LayananTerpaduController::class, 'destroy'])->name('lt.list_delete');
+
+
+    Route::get('/chat_layanan', [ChatController::class, 'chat_guest'])->name('lt.chat_layanan');
+});
+
+Route::middleware(['auth', 'verified', 'can:admin-layanan-terpadu index'])->prefix('terpadu')->group(function () {
+    Route::get('/dashboard', [LayananTerpaduController::class, 'dashboard'])->name('lt.dashboard');
+    Route::get('/admin_riwayat', [LayananTerpaduController::class, 'riwayat_admin'])->name('lt.admin_riwayat');
+    Route::delete('/delete_layanan', [LayananTerpaduController::class, 'destroy'])->name('lt.delete_layanan');
+    Route::put('/update_layanan', [LayananTerpaduController::class, 'update'])->name('lt.update_layanan');
+    // Route::get('/atur_admin_layanan', [LayananTerpaduController::class, 'atur_admin'])->name('lt.atur_admin_layanan');
+    // Route::get('/list_layanan', [LayananTerpaduController::class, 'list_layanan'])->name('lt.list_layanan');
+});
+
+Route::middleware(['auth', 'verified', 'can:layanan-terpadu index'])->prefix('terpadu')->group(function () {
+    Route::put('/update_layanan_forward', [LayananTerpaduController::class, 'forward'])->name('lt.forward');
+    Route::get('/chat_layanan_admin', [ChatController::class, 'chat_admin'])->name('lt.chat_layanan_admin');
+    Route::get('/chat_admin_to_guest/{id}', [ChatController::class, 'chat_admin_guest'])->name('lt.chat_layanan_admin_guest');
+    Route::delete('/atur_admin_layanan', [LayananTerpaduController::class, 'delete_admin'])->name('lt.update_admin_layanan');
+    Route::resource('/atur_admin_layanan', AdminLTController::class)->name('index', 'lt.atur_admin_layanan');
+    Route::resource('/list_layanan', ListLayananController::class)->name('index', 'lt.list_layanan');
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', \App\Livewire\Profile\ProfilePage::class)->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
+
+Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function () {});
